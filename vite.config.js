df3244b3c6a8b02bdd2ed662ebdd921843ea9926
Rarf540 <rrf.arq@gmail.com>
2026-04-29 12:22:10 -0400

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,3 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        landing: 'landing.html',
-        registro: 'registro.html',
-      },
-    },
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/test-setup.js',
-  },
-})
+export default defineConfig({ plugins: [react()] })
